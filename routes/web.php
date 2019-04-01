28934c06a974a5e564387645d4267df46d9feb61
sumon86 <ar1456879@gmail.com>
2019-04-01 12:49:24 +0600

    mastering

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,17 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 //
-//Route::get('/home',[
-//    'uses' => 'HomeController@index',
-//    'as' => 'home'
-//]);
+Route::get('/',[
+    'uses' => 'HomeController@index',
+    'as' => 'home'
+]);
+Route::get('/about',[
+
+    'uses' => 'HomeController@about',
+    'as' => 'about'
+
+]);
+
 //
 //Route::get('/home',function ()
 //{
